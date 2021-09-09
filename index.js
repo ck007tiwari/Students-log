64330b213a33aa772fcf176164a6ac7882650038
ck007tiwari <chandrakishort20@gmail.com>
2021-09-09 12:33:46 +0530

    final edit

--- index.js
+++ index.js
@@ -23,28 +23,34 @@ mongoose.connect('mongodb://localhost:27017/studentLog', {      useNewUrlParser:
       console.log('Mongo Connection error');    
       console.log(err);   
    })
-
+   const purpose = ['UPSC','Competiton','Academic', 'Job_Oreanted', 'Developer'];
+   const goals = ['Achived', 'Working ON', 'Achived_2_Goal'];
    app.get('/',(req,res)=>{
       res.send('Welcome to the Home Page')
    })
 
-   app.get('/students', async (req,res)=>{
-      const students = await Student.find({});
-      console.log(students);
-      res.render('details/details', {students});// here we are sending students  value to " details/details " this location where we can access them.
+   app.get('/students', async (req, res) => {
+      const { purpose } = req.query;
+      if(purpose){
+         const students = await Student.find({ purpose });
+         res.render('details/details', {students, purpose}); 
+      }else{
+         const students = await Student.find({});
+         res.render('details/details', {students, purpose:'All' });// here we are sending students  value to " details/details " this location where we can access them.
+      }
    });
 
    app.get('/students/new',(req,res)=>{
-      res.render('details/newstudent')
+      res.render('details/newstudent', { purpose,goals })
    })
 
    
 
-// test id= 61370969105deb1f9d15b656
+// test id= 61370969105deb1f9d15b656 
    app.get('/students/:id', async (req, res)=>{
       const { id } = req.params
       const student = await Student.findById(id);//Find by id
-      console.log(student);
+      // console.log(student);
       res.render('details/onestudent', {student});
       // res.send('Viewing the detail of one student')
    })
@@ -52,23 +58,30 @@ mongoose.connect('mongodb://localhost:27017/studentLog', {      useNewUrlParser:
    app.get('/students/:id/edit', async (req, res)=>{
       const { id } = req.params
       const student = await Student.findById(id);//Find by id
-      console.log(student);
-      res.render('details/edit', {student});  
+      // console.log(student);
+      res.render('details/edit', {student, purpose, goals});  
 
    })
 //*********************POST DATA*********************** */
    app.post('/students', async (req,res)=>{
       const newStudent = new Student(req.body)
       await newStudent.save();
-      console.log(newStudent); 
+      // console.log(newStudent); 
       // res.send('adding data')
       res.redirect(`students/${newStudent._id}`);
    })
 
+   //editing page submit there data here
  app.put('/students/:id',async(req, res)=>{
    const {id} = req.params;
    const student = await Student.findByIdAndUpdate(id, req.body, {runValidators:true, new:true});//Find by id
    res.redirect(`/students/${student._id}`)
+ })
+
+ app.delete('/students/:id', async(req,res)=>{
+   const { id } = req.params;
+   const delet = await Student.findByIdAndDelete(id);
+   res.redirect('/students');
  })
 
    app.listen(3000, ()=>{
